fcbdf5931c2764bebc7015c38ba49616fa245980
Third Pativeth <126750481+thirdfree2@users.noreply.github.com>
2026-05-11 21:19:30 +0700

    [ENV]

--- lib/utils/api_utils.dart
+++ lib/utils/api_utils.dart
@@ -1,9 +1,9 @@
 class ApiUtils {
-  static const String baseUrl = 'https://kwaythai-service.poonework.com';
+  static const String baseUrl = 'https://kwaythai-service.tsd2024.com';
   // static const String baseUrl = 'http://192.168.1.108:3306';
 
   static const String imageError =
-      'http://kwaythai-service.poonework.com/upload/buffalo/image-1750259700987-680226464.png';
+      'http://kwaythai-service.tsd2024.com/upload/buffalo/image-1750259700987-680226464.png';
 }
 
 // 192.168.1.105
